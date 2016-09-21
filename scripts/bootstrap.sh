251aeb04c14109fd5fa5090490a0b5cfd9731ae3
Joan Fuster <joan.fuster@gmail.com>
2016-09-21 10:05:21 +0200

    Add apt-get update to fix missing packages

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -18,6 +18,7 @@ PART
 echo nameserver 8.8.8.8 > /etc/resolv.conf
 
 # Install Docker requirements
+apt-get update
 apt-get install -y apt-transport-https ca-certificates linux-image-extra-$(uname -r) linux-image-extra-virtual
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo "deb https://apt.dockerproject.org/repo ubuntu-xenial $3" > /etc/apt/sources.list.d/docker.list
