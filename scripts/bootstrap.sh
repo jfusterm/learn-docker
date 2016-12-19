f9690b470c9f0259f199ef24630b5aefbce92c44
Joan Fuster <joan.fuster@gmail.com>
2016-12-19 21:08:50 +0100

    Add iperf3 package

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -19,7 +19,7 @@ echo nameserver 8.8.8.8 > /etc/resolv.conf
 
 # Install Docker requirements
 apt-get update
-apt-get install -y apt-transport-https ca-certificates linux-image-extra-$(uname -r) linux-image-extra-virtual
+apt-get install -y iperf3 apt-transport-https ca-certificates linux-image-extra-$(uname -r) linux-image-extra-virtual
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo "deb https://apt.dockerproject.org/repo ubuntu-xenial $3" > /etc/apt/sources.list.d/docker.list
 apt-get update
